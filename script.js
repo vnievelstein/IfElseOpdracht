cd20b917df01cb79659bcc6dd0f93e162c17ac97
Vera <88088044+vnievelstein@users.noreply.github.com>
2021-08-31 13:47:49 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Opdracht drie deel 1
+alleen als je 18 jaar of ouder bent mag je naar binnen. */
+let age = 19;
+if (age < 18) {
+    console.log("Sorry, je mag niet naar binnen.");
+} else {
+    console.log("Welkom!");
+}
+/* Opdracht drie deel 2
+Alleen vrouwen zijn welkom op de ladiesnight*/
+let isFemale = false;
+if (isFemale) {
+    console.log("Welkom op de ladiesnight.");
+} else {
+    console.log("Dit is alleen voor dames!");
+}
+/* Opdracht 3 deel 3 
+Wie is de Bob?*/
+let driverStatus = "Piet";
+if (driverStatus == "Bob") {
+    console.log("Je kan veilig naar huis rijden.");
+} else {
+    console.log("We zoeken een Bob, zodat je veilig naar huis kan.");
+}
